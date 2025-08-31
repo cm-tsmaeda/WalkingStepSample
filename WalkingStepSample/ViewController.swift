380784b30379587ca169aab4656684ab0e07f2d6
Tasuku Maeda <maeda.tasuku@classmethod.jp>
2025-08-31 17:28:34 +0900

    最初のコミット

---
+++ WalkingStepSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalkingStepSample
+//
+//  Created by Tasuku Maeda on 2025/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WalkingStepSampleTests/WalkingStepSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WalkingStepSampleTests.swift
+//  WalkingStepSampleTests
+//
+//  Created by Tasuku Maeda on 2025/08/31.
+//
+
+import Testing
+@testable import WalkingStepSample
+
+struct WalkingStepSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
