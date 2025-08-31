b0183b752627728e416ea06f427805017318b041
Tasuku Maeda <maeda.tasuku@classmethod.jp>
2025-08-31 18:05:47 +0900

    保存

--- WalkingStepSample/ViewController.swift
+++ WalkingStepSample/ViewController.swift
@@ -6,14 +6,68 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var logLabel: UILabel!
+    @IBOutlet private weak var statusLabal: UILabel!
+    private var pedometer: CMPedometer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statusLabal.text = ""
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func didTapStartButton() {
+        print("start")
+        statusLabal.text = "測定開始しました"
+        if (pedometer == nil) {
+            pedometer = CMPedometer()
+        } else {
+            print("dbg already started")
+            return
+        }
 
+        if (CMPedometer.isStepCountingAvailable()) {
+            print("dbg isStepCountingAvailable")
+            pedometer?.startUpdates(from: Date()) { [weak self] (data, error) in
+                print("dbg update!")
+                guard let self else {
+                    return
+                }
+                if (error != nil) {
+                    print("dbg \(error!.localizedDescription)")
+                    return
+                }
+                guard let data else {
+                    print("dbg data is nil")
+                    return
+                }
+                let steps = data.numberOfSteps
+                let logText = "歩数: \(steps)"
+                print("dbg steps \(steps)")
+                Task { @MainActor in
+                    self.updateLogLabelText(text: logText)
+                }
+            }
+        }
+    }
+    
+    @IBAction func didTapStopButton() {
+        print("stop")
+        if (pedometer == nil) {
+            return
+        }
+        
+        pedometer?.stopUpdates()
+        pedometer = nil
+        statusLabal.text = "測定終了しました"
+    }
+    
+    func updateLogLabelText(text: String?) {
+        logLabel.text = text
+    }
 }
 
